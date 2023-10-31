55db446ee984c5fd6a33782f69d6881724037567
NaryerySalazarD <73444820+NaryerySalazarD@users.noreply.github.com>
2023-10-31 10:24:34 -0600

    Avance naryery

--- MyApplication/app/src/main/java/com/example/myapplication/Inicio.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/Inicio.kt
@@ -1,18 +1,17 @@
 package com.example.myapplication
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class Inicio : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.home_view)
     }
-    fun iniciarSecion(view: View){
-        val intent=Intent(this,Inicio::class.java).apply { }
+    fun regresar(view: View){
+        val intent= Intent(this,MainActivity::class.java).apply { }
         startActivity(intent)
     }
-
 }
