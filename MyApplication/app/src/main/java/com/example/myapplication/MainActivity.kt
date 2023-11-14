25cda3fde66ced03ffb5fc6987c02ceda48ec551
David <garcialfa19@icloud.com>
2023-11-14 15:30:56 -0600

    Login ya funciona, solo falta conectar con DB

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,64 +1,59 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 
-class MainActivity : AppCompatActivity() {
+public class MainActivity : AppCompatActivity() {
+
+    private lateinit var correo: EditText
+    private lateinit var contrasena: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("Estoy en mainActivity")
+        println("Estoy en com.example.myapplication.MainActivity")
 
-        /* val btn: Button = findViewById(R.id.btn_inicio_log_in_view)
-
-         btn.setOnClickListener(object : View.OnClickListener {
-             override fun onClick(v: View?) {
-                 val correo: EditText = findViewById(R.id.texto_Correo_log_in_view)
-                 val contrasena: EditText = findViewById(R.id.texto_contra_log_in_view)
+        // Initialize views
+        correo = findViewById(R.id.texto_Correo_log_in_view)
+        contrasena = findViewById(R.id.texto_contra_log_in_view)
 
         val btn: Button = findViewById(R.id.btn_inicio_log_in_view)
 
-        btn.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                val correo: EditText = findViewById(R.id.texto_Correo_log_in_view)
-                val contrasena: EditText = findViewById(R.id.texto_contra_log_in_view)
-
-
-                 val correo1:String = correo.getText().toString()
-                 val contrasena1:String = contrasena.getText().toString()
-
-                 println("Correo: $correo1")
-                 println("Contrasena: $contrasena1")
-
-             }
-         })*/
+        btn.setOnClickListener {
+            validarCredenciales()
+        }
     }
 
-    fun iniciarSecion(view: View) {
-        println("Estoy en mainActivity")
-        val correo: EditText = findViewById(R.id.texto_Correo_log_in_view)
-        val contrasena: EditText = findViewById(R.id.texto_contra_log_in_view)
+    @SuppressLint("LongLogTag")
+    private fun validarCredenciales() {
+        val correoStr: String = correo.text.toString()
+        val contrasenaStr: String = contrasena.text.toString()
 
+        println("Correo: $correoStr")
+        println("Contraseña: $contrasenaStr")
 
-        val correo1:String = correo.getText().toString()
-        val contrasena1:String = contrasena.getText().toString()
-
-
-        // Imprimir en la consola utilizando Log
-        Log.d("MainActivity", "Correo: $correo1, Contraseña: $contrasena1")
-
-        val intent = Intent(this, Inicio::class.java)
-        startActivity(intent)
-        println("Estoy en mainActivity final")
-
+        // For now, I'll just print a message indicating a successful login.
+        if (validarUsuarioYContrasena(correoStr, contrasenaStr)) {
+            Log.d("com.example.myapplication.MainActivity", "Login successful")
+            val intent = Intent(this, Inicio::class.java)
+            startActivity(intent)
+            println("Estoy en com.example.myapplication.MainActivity final")
+        } else {
+            // Display an error message or handle unsuccessful login
+            Log.d("com.example.myapplication.MainActivity", "Login failed")
+        }
     }
 
-
+    private fun validarUsuarioYContrasena(usuario: String, contrasena: String): Boolean {
+        // For now, I'm just returning true to simulate a successful login.
+        return true
+    }
 
     fun crearCuenta(view: View) {
         val intent = Intent(this, CrearCuenta::class.java)
