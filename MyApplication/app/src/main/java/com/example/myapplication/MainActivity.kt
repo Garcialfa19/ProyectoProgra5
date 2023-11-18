4485d4e91652b1701073ab2cc83d089a31dcc64e
David <garcialfa19@icloud.com>
2023-11-18 02:12:10 -0600

    Login y Registrar ya funcionan
    Despues de agregar un usuario y que meterse al script de PHP para incrementar el idUsuario. Hay que arreglar lo de auto incrementar el idUsuario y creo que hay que cambiar la base de datos para que el idCarrito no de problemas. Conexion y ServicioCliente se eliminan xq ya no se necesitan

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,61 +1,95 @@
 package com.example.myapplication
 
-import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import com.example.myapplication.servicio.ServicioCliente
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONException
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var correo: EditText
-    private lateinit var contrasena: EditText
+    var correo: EditText? = null
+    var contrasena: EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("Estoy en com.example.myapplication.MainActivity")
 
-        // Initialize views
+        // Initialize textfields
         correo = findViewById(R.id.texto_Correo_log_in_view)
         contrasena = findViewById(R.id.texto_contra_log_in_view)
-
+        // Initialize button
         val btn: Button = findViewById(R.id.btn_inicio_log_in_view)
-
-        btn.setOnClickListener {
-            val intent = Intent(this, Inicio::class.java)
-            startActivity(intent)
-        }
-    }
-
-    @SuppressLint("LongLogTag")
-    private fun validarCredenciales() {
-        val correoStr: String = correo.text.toString()
-        val contrasenaStr: String = contrasena.text.toString()
-
-        println("Correo: $correoStr")
-        println("Contraseña: $contrasenaStr")
-
-        var con=ServicioCliente.ConexionBD
-
-        // For now, I'll just print a message indicating a successful login.
-        if (con.validar(correoStr, contrasenaStr)) {
-            Log.d("com.example.myapplication.MainActivity", "Login successful")
-            val intent = Intent(this, Inicio::class.java)
-            startActivity(intent)
-            println("Estoy en com.example.myapplication.MainActivity final")
-        } else {
-            // Display an error message or handle unsuccessful login
-            Log.d("com.example.myapplication.MainActivity", "Login failed")
-        }
     }
 
     fun crearCuenta(view: View) {
         val intent = Intent(this, CrearCuenta::class.java)
         startActivity(intent)
+    }
+
+    fun login(view: View) {
+        val url = "http://192.168.0.105/ecomerce/login.php"
+
+        val queue = Volley.newRequestQueue(this)
+
+        val correoText = correo?.text.toString()
+        val contrasenaText = contrasena?.text.toString()
+
+        // Log the altered URL
+        val alteredUrl = "$url?correo=$correoText&contrasena=$contrasenaText"
+        Log.d("URL_LOG", "Altered Request URL: $alteredUrl")
+
+        val stringRequest = StringRequest(
+            Request.Method.GET,
+            alteredUrl,
+            { response ->
+                // Log the entire response for debugging
+                Log.d("FULL_RESPONSE", response)
+
+                // Parse the JSON response
+                try {
+                    val jsonResponse = JSONObject(response)
+                    val success = jsonResponse.getBoolean("success")
+
+                    if (success) {
+                        // Authentication successful
+                        Toast.makeText(this, "Usuario reconocido", Toast.LENGTH_LONG).show()
+
+                        // Extract client data if needed
+                        val clienteJson = jsonResponse.getJSONObject("cliente")
+
+                        // You can use the client data as needed
+
+                        // Start the new activity
+                        val intent = Intent(this, Inicio::class.java)
+                        intent.putExtra("correo", correoText.toString())
+                        startActivity(intent)
+                    } else {
+                        // Authentication failed
+                        val message = jsonResponse.getString("message")
+                        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+                    }
+                } catch (e: JSONException) {
+                    // Handle JSON parsing error
+                    Log.e("JSON_ERROR", "Error parsing JSON", e)
+                    Toast.makeText(this, "Error parsing JSON", Toast.LENGTH_LONG).show()
+                }
+            },
+            { error ->
+                // Handle error
+                Log.e("VOLLEY_ERROR", "Error: ${error.networkResponse?.statusCode}", error)
+                Toast.makeText(this, "Error ${error.networkResponse?.statusCode}", Toast.LENGTH_LONG).show()
+            })
+
+        queue.add(stringRequest)
     }
 }
