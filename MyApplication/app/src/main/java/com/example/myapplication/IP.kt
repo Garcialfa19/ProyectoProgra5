42111e72a17ce1671f0a7ceaf1e0f777e6eddef0
David <garcialfa19@icloud.com>
2023-11-22 17:41:47 -0600

    .

--- MyApplication/app/src/main/java/com/example/myapplication/IP.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/IP.kt
@@ -3,32 +3,35 @@ import java.net.NetworkInterface
 
 class IP {
 
-fun getIpv4Address(): String {
-    var ipv4Address = "Unknown"
+    fun getIpv4Address(): String {
+        var ipv4Address = "Unknown"
 
-    try {
-        val interfaces = NetworkInterface.getNetworkInterfaces()
-        while (interfaces.hasMoreElements()) {
-            val networkInterface = interfaces.nextElement()
-            val addresses = networkInterface.inetAddresses
+        try {
+            val interfaces = NetworkInterface.getNetworkInterfaces()
+            while (interfaces.hasMoreElements()) {
+                val networkInterface = interfaces.nextElement()
+                val addresses = networkInterface.inetAddresses
 
-            while (addresses.hasMoreElements()) {
-                val address = addresses.nextElement()
-                if (!address.isLoopbackAddress && address is InetAddress && address.hostAddress.contains(".")) {
-                    // Check if the address is IPv4
-                    ipv4Address = address.hostAddress
+                while (addresses.hasMoreElements()) {
+                    val address = addresses.nextElement()
+                    if (!address.isLoopbackAddress && address is InetAddress && address.hostAddress.contains(
+                            "."
+                        )
+                    ) {
+                        // Check if the address is IPv4
+                        ipv4Address = address.hostAddress
+                    }
                 }
             }
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
-    } catch (e: Exception) {
-        e.printStackTrace()
+
+        return ipv4Address
     }
 
-    return ipv4Address
-}
-
-fun main() {
-    val ipv4Address = getIpv4Address()
-    println("Device IPv4 Address: $ipv4Address")
-}
+    fun main() {
+        val ipv4Address = getIpv4Address()
+        println("Device IPv4 Address: $ipv4Address")
+    }
 }
